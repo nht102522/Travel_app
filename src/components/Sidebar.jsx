ed0d7d739d19f72cef3ee2d8a5187a94075985f9
Nam Trinh <nht35@drexel.edu>
2026-09-20 20:46:41 -0400

    built pages

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,5 @@
+function Sidebar() {
+  return <div className=""></div>;
+}
+
+export default Sidebar;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index_copy.css";
 import "./App.css";
 import App from "./App.jsx";
+import { AuthProvider } from "./contexts/FakeAuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
+  ``,
 );
